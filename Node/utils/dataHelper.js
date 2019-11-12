5e44df50a121ba0d013ea8b80a33d2e67b5d88c4
wanghuan <huan.wang@chiefclouds.com>
2019-11-12 14:23:18 +0800

    Updat

--- Node/utils/dataHelper.js
+++ Node/utils/dataHelper.js
@@ -1,11 +1,9 @@
-// 二维数组变一维
 export const MatrixToArr = function (matrix) {
-    var result = [];
-    matrix.map(row => (result = result.concat(row)));
+    var result = []; 
+    matrix.map(row => (result = result.concat(row))); 
     return result; 
 }
 
-// 一维数组变二维
 export const ArrToMatrix = function (arr, rowLength) {
     var matrix = [];
     while(arr.length) {
@@ -14,7 +12,6 @@ export const ArrToMatrix = function (arr, rowLength) {
     return matrix;
 }
 
-// 去重
 export const FilterForRemoveSame = function (arr) {
     var arrMap = {}, result = [];
     // arr.map(data => (arrMap[data] = true));
@@ -23,4 +20,26 @@ export const FilterForRemoveSame = function (arr) {
         arrMap[arr[i]] = true;
     }
     return result;
+}
+
+export const ArrToMap = function (arr, objKey) {
+    var mapData = {};
+    arr && arr.map(data => ( mapData[ objKey ? data[objKey] : data ] = data ));
+    return mapData;
+}
+
+export const CrossJoinArr = function (leftArr, rightArr, objKey) {
+    var leftMap = ArrToMap(leftArr, objKey), rightMap = ArrToMap(rightArr, objKey);
+
+    var result = { left: [], cross: [], right: [] };
+    result.left = leftArr.filter(item => !rightMap[objKey ? item[objKey] : item]);
+    result.cross = leftArr.filter(item => rightMap[objKey ? item[objKey] : item]);
+    result.right = rightArr.filter(item => !leftMap[objKey ? item[objKey] : item]);
+    return result;
+}
+
+export const RightJoinArr = function (leftArr, rightArr, objKey) {
+    var crossResult = CrossJoinArr(leftArr, rightArr, objKey);
+
+    return { result: crossResult.cross.concat(crossResult.right), rest: crossResult.left };
 }
