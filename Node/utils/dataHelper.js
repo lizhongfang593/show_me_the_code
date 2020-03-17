f2aec02778188ef0f8e1bc898e24567aa7a1b697
wanghuan <huan.wang@chiefclouds.com>
2020-03-17 18:04:40 +0800

    注释

--- Node/utils/dataHelper.js
+++ Node/utils/dataHelper.js
@@ -1,9 +1,13 @@
+// 数组二维变一维
+// MatrixToArr([ [1,2,3], [4,5,6], [7,8] ]) = [ 1,2,3,4,5,6,7,8 ]
 export const MatrixToArr = function (matrix) {
     var result = []; 
     matrix.map(row => (result = result.concat(row))); 
     return result; 
 }
 
+// 数组一维变二维 
+// ArrToMatrix([ 1,2,3,4,5,6,7,8 ], 3) = [ [1,2,3], [4,5,6], [7,8] ]
 export const ArrToMatrix = function (arr, rowLength) {
     var matrix = [];
     while(arr.length) {
@@ -12,6 +16,7 @@ export const ArrToMatrix = function (arr, rowLength) {
     return matrix;
 }
 
+// 数组去重
 export const FilterForRemoveSame = function (arr) {
     var arrMap = {}, result = [];
     // arr.map(data => (arrMap[data] = true));
@@ -22,12 +27,21 @@ export const FilterForRemoveSame = function (arr) {
     return result;
 }
 
+// 数组 转 Map :  
+// ArrToMap([{id: 'I_1', name: 'nnnn'}, {id: 'I_2', name: 'nnnn2'}], 'id') = { 'I_1': {id: 'I_1', name: 'nnnn'}, 'I_2': {id: 'I_2', name: 'nnnn2'} }
 export const ArrToMap = function (arr, objKey) {
     var mapData = {};
     arr && arr.map(data => ( mapData[ objKey ? data[objKey] : data ] = data ));
     return mapData;
 }
 
+// Map 转 数组
+// MapToArr({ 'I_1': {id: 'I_1', name: 'nnnn'}, 'I_2': {id: 'I_2', name: 'nnnn2'} }) = [{id: 'I_1', name: 'nnnn'}, {id: 'I_2', name: 'nnnn2'}];
+export const MapToArr = function (obj) {
+    return obj && Object.keys(obj).map(key => obj[key]);
+}
+
+// 对象数组取交集
 export const CrossJoinArr = function (leftArr, rightArr, objKey) {
     var leftMap = ArrToMap(leftArr, objKey), rightMap = ArrToMap(rightArr, objKey);
 
@@ -38,6 +52,7 @@ export const CrossJoinArr = function (leftArr, rightArr, objKey) {
     return result;
 }
 
+// 对象数组 Right Join
 export const RightJoinArr = function (leftArr, rightArr, objKey) {
     var crossResult = CrossJoinArr(leftArr, rightArr, objKey);
 
